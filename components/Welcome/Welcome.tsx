2a6ffd41e10b416612594ce55d8931b72be1a6cf
lysi <lisy@nupace.app>
2023-11-13 10:12:58 -0800

    added user setting and community config

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,7 +1,6 @@
 import { Container, Select, RangeSlider, Slider, TextInput, Paper, Checkbox } from '@mantine/core';
- import classes from './Welcome.module.css';
+import classes from './Welcome.module.css';
 import React, { useState } from 'react';
-
 
 export function Welcome() {
   // const form = useForm({
@@ -17,296 +16,192 @@ export function Welcome() {
   const [values, setValues] = useState([20, 1500]);
   const formatThumbLabel = (value) => `$${value}`;
 
- 
   return (
     <>
+      <Paper radius="md" p="xl" withBorder className={classes.paperContainer}>
+        <Container size="m">
+          <h1 className={classes.h1}>Superuser Register a New User</h1>
 
+          <TextInput
+            label="AppUserId"
+            placeholder="isnt this automaically created?"
+            classNames={classes}
+          />
 
-
-<Paper radius="md" p="xl"  withBorder className={classes.paperContainer}>
-
-<Container size="m" >
-
-<h1 className={classes.h1}>Community Configuration</h1>
-
-    <TextInput label="CommunityID" placeholder="9ab23251-8c65-4142-9769-ce35eb048bb4" classNames={classes} />
-    <TextInput label="CommunityApprovedSigner" placeholder="Jane Smith" classNames={classes} />
-    
-    <TextInput
-          label="LawyerFirm"
-          placeholder="Puckett & Redford"
-          // {...form.getInputProps('lawyer.practiceName')}
-        />
-
-        <TextInput
-          label="LawyerName"
-          placeholder="Alice Smith"
-          mt="md"
-          // {...form.getInputProps('lawyer.name')}
-        />
- 
-        <TextInput
-          label="LawyerPhone"
-          placeholder="(415) 613-3033"
-          mt="md"
-          // {...form.getInputProps('lawyer.phone')}
-        />
-
-      <TextInput
-          label="LawyerEmail"
-          placeholder="alice@kendallaw.com"
-          mt="md"
-          // {...form.getInputProps('lawyer.email')}
-        />
-  
- <TextInput
-          label="LawyerAddress"
-          placeholder="4331 Lake Washington Blvd NE, Suite 200 Kirkland, WA 98033"
-          mt="md"
-          // {...form.getInputProps('lawyer.address')}
-        />
-
-
-
+          <TextInput
+            label="App User Email"
+            placeholder="jane.smith@avenue5.aom"
+            classNames={classes}
+          />
+{/* Role */}
 <Select
-      label="ImportReportSource"
-      placeholder="Pick value"
-      data={['Yardi', 'Real Page', 'Other']}
-    />
+            mt="md"
+            comboboxProps={{ withinPortal: true }}
+            data={[
+              'On-site Property Manager',
+              'Assistant Property Manager',
+              'Operations Manager',
+              'Regional Property Manager',
+              'Owner',
+              'Other',
+            ]}
+            placeholder="Pick one"
+            label="Who will be using Nupace to run notices?"
+            classNames={classes}
+          />
 
+          <TextInput label="PmorgId" placeholder="89827675276898" classNames={classes} />
 
- 
+          <TextInput label="PmorgName" placeholder="Avenue5 Residential" classNames={classes} />
 
+          <TextInput label="FirstName" placeholder="Jane" classNames={classes} />
 
-<Checkbox
-      defaultChecked
-      label="Generate small balance notices?"
-      my="md"
-      
-    />
+          <TextInput label="LastName" placeholder="Smith" classNames={classes} />
 
-{/* DelinquentAmount
-SmallBalanceAmount */}
+          <TextInput
+            label="CommunityID"
+            placeholder="9ab23251-8c65-4142-9769-ce35eb048bb4"
+            classNames={classes}
+          />
 
- <p my="lg">
-        <b>Minimum and maximum for the small balance notices</b>
- </p>
- <RangeSlider
-        min={20}
-        max={1500}
-        value={values}
-        onChange={setValues}
-        labelAlwaysOn
-        marks={[
-          { value: 20, label: '$20' },
-          { value: 1500, label: '$1500' }
-        ]}
-        thumbLabel={formatThumbLabel}
-        my="lg"
-        py="lg"
-      />
+          <TextInput label="CommunityName" placeholder="Camrose Apartments" classNames={classes} />
 
+          <TextInput label="CommunityName" placeholder="Camrose Apartments" classNames={classes} />
 
-<Checkbox
-      defaultChecked
-      label="Subsidy tenants"
-      my="md"
-    />
+          <TextInput label="CommunityCode" placeholder="TBD" classNames={classes} />
 
-<Checkbox
-      defaultChecked
-      label="CommunityHasMultiAddress"
-      my="md"
-    />
+          <TextInput label="OwnerId" placeholder="TBD" classNames={classes} />
 
-
-<Checkbox
-      defaultChecked
-      label="PaymentPlanOfferForAll"
-      my="md"
-    />
-
- 
- 
- 
-
-</Container>
-
-</Paper>
-
-
-    <Paper radius="md" p="xl" withBorder className={classes.paperContainer}>
-
-      <Container size="m"   >
-
-      <h1 className={classes.h1}>Community Address</h1>
-
-        <TextInput label="Name" placeholder="Camrose Apartments" classNames={classes} />
-
-        <TextInput
-          label="Address"
-          placeholder="65 Camrose Ave"
-          // {...form.getInputProps('community.address')}
-        />
-        <TextInput
-          label="City"
-          placeholder="Kirkland"
-          mt="md"
-          // {...form.getInputProps('community.city')}
-        />
- 
-        <TextInput
-          label="State"
-          placeholder="Washington"
-          mt="md"
-          // {...form.getInputProps('community.city')}
-        />
-
-      <TextInput
-          label="Zip"
-          placeholder="98033"
-          mt="md"
-          // {...form.getInputProps('community.city')}
-        />
-
-<Checkbox
-      defaultChecked
-      label="Multiple addresses"
-      mt="md"
-    />
-
-        
-      </Container>
-    
-    </Paper>
-
-
-    <Paper radius="md" p="xl"  withBorder className={classes.paperContainer}>
-
-      <Container size="m" >
-
-      <h1 className={classes.h1}>Community Settings</h1>
-
-      <TextInput label="Residence code" placeholder="pn10666" classNames={classes} />
-
-      <TextInput label="Community email" placeholder="camrose@avenue5.com" classNames={classes} />
-
-      <TextInput label="Community phone" placeholder="(206) 887-3723" classNames={classes} />
-
-
-
-    
-
-
-  
-
-      </Container>
-
+          <TextInput label="OwnerName" placeholder="TBD" classNames={classes} />
+        </Container>
       </Paper>
 
-    <Paper radius="md" p="xl"  withBorder className={classes.paperContainer}>
+      <Paper radius="md" p="xl" withBorder className={classes.paperContainer}>
+        <Container size="m">
+          <h1 className={classes.h1}>Superuser Configure the Community </h1>
 
-<Container size="m" >
+          <TextInput
+            label="communityId"
+            placeholder="9ab23251-8c65-4142-9769-ce35eb048bb4"
+            classNames={classes}
+          />
 
-<h1 className={classes.h1}>User Settings</h1>
+          <TextInput
+            label="communityApprovedSigner"
+            placeholder="Julia SMith"
+            classNames={classes}
+          />
 
-    <TextInput label="Property management company" placeholder="Avenue5" classNames={classes} />
+          <TextInput
+            label="LawyerFirm"
+            placeholder="Puckett & Redford"
+            // {...form.getInputProps('lawyer.practiceName')}
+          />
 
+          <TextInput
+            label="LawyerName"
+            placeholder="Alice Smith"
+            mt="md"
+            // {...form.getInputProps('lawyer.name')}
+          />
 
-<Select
-          mt="md"
-          comboboxProps={{ withinPortal: true }}
-          data={['On-site Property Manager', 'Assistant Property Manager', 'Operations Manager', 'Regional Property Manager', 'Owner', 'Other']}
-          placeholder="Pick one"
-          label="Who will be using Nupace to run notices?"
-          classNames={classes}
-        />
+          <TextInput
+            label="LawyerAddress"
+            placeholder="4331 Lake Washington Blvd NE, Suite 200 Kirkland, WA 98033"
+            mt="md"
+            // {...form.getInputProps('lawyer.address')}
+          />
 
+          <TextInput
+            label="LawyerPhone"
+            placeholder="(415) 613-3033"
+            mt="md"
+            // {...form.getInputProps('lawyer.phone')}
+          />
 
-    <TextInput
-          label="First name"
-          placeholder="Lisy"
-          // {...form.getInputProps('user.firstName')}
-        />
-        <TextInput
-          label="Last name"
-          placeholder="ruiz"
-          mt="md"
-          // {...form.getInputProps('user.lastName')}
-        />
- 
-        <TextInput
-          label="Phone"
-          placeholder="(425) 654-1022"
-          mt="md"
-          // {...form.getInputProps('user.phone')}
-        />
+          <TextInput
+            label="LawyerEmail"
+            placeholder="alice@kendallaw.com"
+            mt="md"
+            // {...form.getInputProps('lawyer.email')}
+          />
 
-      <TextInput
-          label="Email"
-          placeholder="pm.camrose@avenue5.com"
-          mt="md"
-          // {...form.getInputProps('user.email')}
-        />
- 
+          <Checkbox defaultChecked label="BundleAllDocuments?" my="md" />
 
-</Container>
+          <Checkbox defaultChecked label="communityHasSection8?" my="md" />
 
-</Paper>
+          <Checkbox defaultChecked label="communityHasMultiAddress" my="md" />
 
+          <Checkbox defaultChecked label="Subsidy tenants" my="md" />
 
-<Paper radius="md" p="xl"  withBorder className={classes.paperContainer}>
+          <Checkbox defaultChecked label="tenantFriendlyRepayment" my="md" />
 
-<Container size="m" >
+          <Checkbox defaultChecked label="PaymentPlanOfferForAll" my="md" />
 
-<h1 className={classes.h1}>Attorney Settings</h1>
+          <p my="lg">
+            <b>Minimum and maximum for the small balance notices</b>
+          </p>
 
-    <TextInput
-          label="Practice name"
-          placeholder="Lisy"
-          // {...form.getInputProps('lawyer.practiceName')}
-        />
-        <TextInput
-          label="Name"
-          placeholder="Alice Smith"
-          mt="md"
-          // {...form.getInputProps('lawyer.name')}
-        />
- 
-        <TextInput
-          label="Phone"
-          placeholder="(415) 613-3033"
-          mt="md"
-          // {...form.getInputProps('lawyer.phone')}
-        />
+          {/* delinquentAmount + smallBalanceAmount */}
+          <RangeSlider
+            min={20}
+            max={1500}
+            value={values}
+            onChange={setValues}
+            labelAlwaysOn
+            marks={[
+              { value: 20, label: '$20' },
+              { value: 1500, label: '$1500' },
+            ]}
+            thumbLabel={formatThumbLabel}
+            my="lg"
+            py="lg"
+          />
 
-      <TextInput
-          label="Email"
-          placeholder="alice@kendallaw.com"
-          mt="md"
-          // {...form.getInputProps('lawyer.email')}
-        />
- 
- <TextInput
-          label="Address"
-          placeholder="4331 Lake Washington Blvd NE, Suite 200 Kirkland, WA 98033"
-          mt="md"
-          // {...form.getInputProps('lawyer.address')}
-        />
+          <Select
+            label="ImportReportSource"
+            placeholder="Pick value"
+            data={['Yardi', 'Real Page', 'Other']}
+          />
 
-</Container>
+          <Checkbox defaultChecked label="GenerateCaresAct" my="md" />
 
-</Paper>
+          <Checkbox defaultChecked label="GenerateERPP" my="md" />
 
+          <Checkbox defaultChecked label="GenerateSmallBalance?" my="md" />
 
+          <Checkbox defaultChecked label="TenantFriendlyRepayment?" my="md" />
 
+          <TextInput
+            label="Address"
+            placeholder="65 Camrose Ave"
+            // {...form.getInputProps('community.address')}
+          />
+          <TextInput
+            label="City"
+            placeholder="Kirkland"
+            mt="md"
+            // {...form.getInputProps('community.city')}
+          />
 
+          <TextInput
+            label="State"
+            placeholder="Washington"
+            mt="md"
+            // {...form.getInputProps('community.city')}
+          />
 
+          <TextInput
+            label="Zip"
+            placeholder="98033"
+            mt="md"
+            // {...form.getInputProps('community.city')}
+          />
 
+         </Container>
+      </Paper>
 
-
-     </>
+   
+    </>
   );
 }
-
-
