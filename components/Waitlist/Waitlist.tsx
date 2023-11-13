05a586a253663a33df8630e0f6f8e07e998c32f9
lysi <lisy@nupace.app>
2023-11-13 13:20:48 -0800

    waitlist draft

--- components/Waitlist/Waitlist.tsx
+++ components/Waitlist/Waitlist.tsx
@@ -4,24 +4,7 @@ import React from 'react';
 
 export function Waitlist() {
   return (
-    //     <Container style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-    //     <Title order={2}>Join Our Waitlist</Title>
-    //     <Text size="md" align="center" style={{ marginTop: '1rem' }}>
-    //       Sign up to get early access to our exclusive offers and updates.
-    //     </Text>
-
-    //     <Group align="center" spacing="sm" style={{ marginTop: '2rem', width: '100%', maxWidth: '600px' }}>
-    //       <TextInput
-    //         placeholder="Your email"
-    //         required
-    //         style={{ flexGrow: 1 }}
-    //       />
-    //       <Button variant="filled">
-    //         Join the Waitlist
-    //       </Button>
-    //     </Group>
-    //   </Container>
-
+   
     <Container
       style={{
         display: 'flex',
@@ -31,17 +14,25 @@ export function Waitlist() {
         height: '100vh',
       }}
     >
-      <Title order={2}>Join Our Waitlist</Title>
-      <Text size="md" align="center" style={{ marginTop: '1rem' }}>
-        Sign up to get early access to our exclusive offers and updates.
+      <Title order={1}>Launching soon</Title>
+
+      <Text fz="lg" lh="md" align="center" style={{ marginTop: '1rem', maxWidth: '800px' }}>
+        We're onboarding properties in early 2024. Sign up for early access!
       </Text>
+
       <Group
         align="center"
         spacing="sm"
-        style={{ marginTop: '2rem', width: '100%', maxWidth: '500px' }}
+        style={{ marginTop: '2rem', width: '100%', maxWidth: '550px' }}
       >
         <TextInput placeholder="Your email" required style={{ flexGrow: 1 }} />
-        <Button variant="filled">Join the Waitlist</Button>
+
+        <Button
+          variant="gradient"
+          gradient={{ from: 'blue', to: 'rgba(33, 171, 235, 1)', deg: 185 }}
+        >
+          Join the waitlist
+        </Button>
       </Group>
     </Container>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,9 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Waitlist } from '../components/Waitlist/Waitlist';
-
 
 export default function HomePage() {
   return (
     <>
-      {/* <Welcome /> */}
       <Waitlist />
-
-
-
-
- 
-       {/* <ColorSchemeToggle /> */}
-       
-        </>
+    </>
   );
 }
-
- 
